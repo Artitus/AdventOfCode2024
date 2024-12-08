a56dcd599c10473f41eba9e7133fa68e5e9acd55
Artitus <artituswork@gmail.com>
2024-12-07 21:18:46 -0600

    Run all of the days

--- src/main/java/io/hurbs/adventofcode2024/AdventMain.java
+++ src/main/java/io/hurbs/adventofcode2024/AdventMain.java
@@ -13,29 +13,29 @@ public class AdventMain
     public static void main( String[] args ) throws IOException
     {
         System.out.println( "Hello World!" );
-        //AdventPuzzleDayOne day1 = new AdventPuzzleDayOne("day1input.txt");
-        //System.out.println("Day 1, part 1: " + day1.solvePart1());
-        //System.out.println("Day 1, part 2: " + day1.solvePart2());
+        AdventPuzzleDayOne day1 = new AdventPuzzleDayOne("day1input.txt");
+        System.out.println("Day 1, part 1: " + day1.solvePart1());
+        System.out.println("Day 1, part 2: " + day1.solvePart2());
 
-       // AdventPuzzleDayTwo day2 = new AdventPuzzleDayTwo("day2input.txt");
-       // System.out.println("Day 2, part 1: " + day2.solvePart1());
-       // System.out.println("Day 2, part 2: " + day2.solvePart2());
+        AdventPuzzleDayTwo day2 = new AdventPuzzleDayTwo("day2input.txt");
+        System.out.println("Day 2, part 1: " + day2.solvePart1());
+        System.out.println("Day 2, part 2: " + day2.solvePart2());
 
-        //AdventPuzzleDayThree day3 = new AdventPuzzleDayThree("day3input.txt");
-        //System.out.println("Day 3, part 1 " + day3.solvePart1());
-        //System.out.println("Day 3, part 2 " + day3.solvePart2());
+        AdventPuzzleDayThree day3 = new AdventPuzzleDayThree("day3input.txt");
+        System.out.println("Day 3, part 1 " + day3.solvePart1());
+        System.out.println("Day 3, part 2 " + day3.solvePart2());
 
-        //AdventPuzzleDayFour day4 = new AdventPuzzleDayFour("day4input.txt");
-        //System.out.println("Day 4, part 1 " + day4.solvePart1());
-        //System.out.println("Day 4, part 2 " + day4.solvePart2());
+        AdventPuzzleDayFour day4 = new AdventPuzzleDayFour("day4input.txt");
+        System.out.println("Day 4, part 1 " + day4.solvePart1());
+        System.out.println("Day 4, part 2 " + day4.solvePart2());
 
-        //AdventPuzzleDayFive day5 = new AdventPuzzleDayFive("day5input.txt");
-        //System.out.println("Day 5, part 1 " + day5.solvePart1());
-        //System.out.println("Day 5, part 2 " + day5.solvePart2());
+        AdventPuzzleDayFive day5 = new AdventPuzzleDayFive("day5input.txt");
+        System.out.println("Day 5, part 1 " + day5.solvePart1());
+        System.out.println("Day 5, part 2 " + day5.solvePart2());
 
-        //AdventPuzzleDaySix day6 = new AdventPuzzleDaySix("day6input.txt");
-        //System.out.println("Day 6, part 1 " + day6.solvePart1());
-        //System.out.println("Day 6, part 2 " + day6.solvePart2());
+        AdventPuzzleDaySix day6 = new AdventPuzzleDaySix("day6input.txt");
+        System.out.println("Day 6, part 1 " + day6.solvePart1());
+        System.out.println("Day 6, part 2 " + day6.solvePart2());
 
         AdventPuzzleDaySeven day7 = new AdventPuzzleDaySeven("day7input.txt");
         System.out.println("Day 7, part 1: " + day7.solvePart1());
